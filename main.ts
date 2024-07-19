9e4667924fd1a5737b22f3df475fe17a70b0daf0
pvtryan88 <ryan.corker@gmail.com>
2024-07-19 19:43:06 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,23 @@
-let Wisp = sprites.create(img`
-    . . . f f f . . . . . . . . . . 
-    . . f 2 2 2 f f . . . . . . . . 
-    . . f 2 4 2 2 2 f f . . . . . . 
-    . . f 2 2 4 4 4 2 2 f . . . . . 
-    . . . f 2 2 4 4 4 2 2 f . . . . 
-    . . . f 2 2 4 4 4 4 4 2 f . . . 
-    . . . . f 2 4 4 5 5 5 5 2 f . . 
-    . . . f 2 4 4 5 f 5 f 5 4 2 f . 
-    . . . f 2 4 4 5 f 5 f 5 4 2 f . 
-    . . . f 2 4 4 5 f 5 f 5 4 2 f . 
-    . . . f 2 2 4 5 5 5 5 5 4 2 f . 
-    . . . f 2 2 4 4 5 5 5 4 4 2 f . 
-    . . . . f 2 2 4 4 4 4 4 2 f . . 
-    . . . . . f 2 2 2 2 2 2 f . . . 
-    . . . . . f 2 f . f 2 f . . . . 
-    . . . . . f 2 2 f f 2 2 f . . . 
-    `, SpriteKind.Player)
+function Start_Game () {
+    scene.setBackgroundColor(9)
+    Wisp = sprites.create(img`
+        . . . f f f . . . . . . . . . . 
+        . . f 2 2 2 f f . . . . . . . . 
+        . . f 2 4 2 2 2 f f . . . . . . 
+        . . f 2 2 4 4 4 2 2 f . . . . . 
+        . . . f 2 2 4 4 4 2 2 f . . . . 
+        . . . f 2 2 4 4 4 4 4 2 f . . . 
+        . . . . f 2 4 4 5 5 5 5 2 f . . 
+        . . . f 2 4 4 5 f 5 f 5 4 2 f . 
+        . . . f 2 4 4 5 f 5 f 5 4 2 f . 
+        . . . f 2 4 4 5 f 5 f 5 4 2 f . 
+        . . . f 2 2 4 5 5 5 5 5 4 2 f . 
+        . . . f 2 2 4 4 5 5 5 4 4 2 f . 
+        . . . . f 2 2 4 4 4 4 4 2 f . . 
+        . . . . . f 2 2 2 2 2 2 f . . . 
+        . . . . . f 2 f . f 2 f . . . . 
+        . . . . . f 2 2 f f 2 2 f . . . 
+        `, SpriteKind.Player)
+}
+let Wisp: Sprite = null
+Start_Game()
